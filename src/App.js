8ab106add8ed5076a2022ac96c3a1482148e4d5b
Kangonga <guchu413@gmail.com>
2022-10-01 03:07:59 +0300

    complete tabbing process

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import NavBar from './NavBar';
 import Mid from './Mid';
 import Contacts from './Contacts';
 import Menu from './Menu';
+import { useState } from 'react';
 
 function App() {
+  // const[bgcCover,setBgCover] = useState("cover");
+  const [currentTab, setTab] = useState("Home");
   return (
-    <>
-      <NavBar />
-      {/* <Mid /> */}
-      <Contacts/>
-      {/* <Menu/> */}
-    </>
+    <div className={currentTab=="Menu"?"contain":"cover"}>
+      <NavBar currentTab = {currentTab} setTab = {setTab}/>
+      { currentTab=="Home" && <Mid />}
+      {currentTab=="Contacts" &&  <Contacts/>}
+      { currentTab=="Menu" && <Menu/> }
+    </div>
   );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-export default function NavBar(){
+export default function NavBar({setTab, currentTab}){
+    function handleClick(event){
+        setTab(()=>event.target.textContent)
+        // if (currentTab === "Menu")setBgCover(()=>"contain")
+        // else (setBgCover(()=>"cover"))
+      }
     return(
         <div className="navbar">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Contacts</li>
+            <li onClick={handleClick}>Home</li>
+            <li onClick={handleClick}>Menu</li>
+            <li onClick={handleClick}>Contacts</li>
         </div>
     )
 }
