a507610d56126cd38147cdaa842c78d135f13645
Kangonga <guchu413@gmail.com>
2022-10-03 23:46:34 +0300

    fixed default route

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar({setTab, currentTab}){
       }
     return(
         <div className="navbar">
-            <Link className="nav"to ="/" onClick={handleClick}>Home</Link>
+            <Link className="nav"to ="home" onClick={handleClick}>Home</Link>
             <Link className="nav"to="menu"onClick={handleClick}>Menu</Link>
             <Link className="nav"to="contacts"onClick={handleClick}>Contacts</Link>
             <Link className="nav"to="reservations"onClick={handleClick}>Reservations</Link>
